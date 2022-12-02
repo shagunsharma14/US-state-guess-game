0c3447639bbd6d76045b3b1a778a5c86f9132e20
GeekyNerd <85250765+BloodShoT14@users.noreply.github.com>
2022-12-02 21:59:16 +0530

    Add files via upload

---
+++ us-states-game-start/main.py
@@ -0,0 +1,46 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. Quiz Game")
+
+# To add new shape in the turtle
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for states in all_states:
+            if states not in guessed_states:
+                missing_states.append(states)
+        # states to learn.csv
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x),int(state_data.y))
+        t.write(answer_state)
+
+
+# To get the coordinates
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+
+
